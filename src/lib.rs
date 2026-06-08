8342bdd44065f5d86e7408d2e77e76c464f3067b
kyotalab <jagbvg.eptpeox@gmail.com>
2026-06-08 21:58:45 +0900

    feat: add module to each mod.rs and lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod app;
+pub mod config;
+pub mod error;
+pub mod model;
+pub mod search;
+pub mod store;
+pub mod ui;
---
+++ src/ui/mod.rs
@@ -0,0 +1,5 @@
+pub mod layout;
+pub mod preview;
+pub mod scratch;
+pub mod wizard;
+pub mod zettelkasten;
