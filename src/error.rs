e35a849ec4de15d2e4122d195612f027f553159f
kyotalab <jagbvg.eptpeox@gmail.com>
2026-06-10 22:01:12 +0900

    feat: add KtermError Varients

--- src/error.rs
+++ src/error.rs
@@ -4,5 +4,12 @@ use thiserror::Error;
 pub enum KtermError {
     #[error("Unknown CardStatus: {0}")]
     UnknownCardStatus(String),
-    // 他のモジュールのエラーも追加していく
+    #[error("Config read error: {0}")]
+    ConfigReadError(String),
+    #[error("Config parse error: {0}")]
+    ConfigParseError(String),
+    #[error("Config write error: {0}")]
+    ConfigWriteError(String),
+    #[error("Setup error: {0}")]
+    SetupError(String),
 }
