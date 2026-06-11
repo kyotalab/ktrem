bb09423c8e49ecd39e5bb3cf877ec90edd1f0023
kyotalab <jagbvg.eptpeox@gmail.com>
2026-06-11 09:02:31 +0900

    feat: add function(id_from_file_name)

--- src/model/note.rs
+++ src/model/note.rs
@@ -1,5 +1,6 @@
 use chrono::{DateTime, Utc};
 
+#[derive(Clone)]
 pub struct Zettel {
     pub id: String,        // "1/1/2"（表示ID）
     pub file_name: String, // "1-1-2"（ファイル名、拡張子なし）
@@ -14,8 +15,13 @@ impl Zettel {
         // contentからH1を抽出して返す
         extract_h1(&self.content).unwrap_or_else(|| "Untitled".to_string())
     }
+
+    pub fn id_from_file_name(file_name: &str) -> String {
+        file_name.replace('-', "/")
+    }
 }
 
+#[derive(Clone)]
 pub struct Scratch {
     pub timestamp: String, // "202506061430"
     pub content: String,   // 本文全体
