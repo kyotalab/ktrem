62478a4866f0f968056cbe851a7db6c1f8aaec50
kyotalab <jagbvg.eptpeox@gmail.com>
2026-06-11 09:08:01 +0900

    feat: add KtermError variants

--- src/error.rs
+++ src/error.rs
@@ -12,4 +12,14 @@ pub enum KtermError {
     ConfigWriteError(String),
     #[error("Setup error: {0}")]
     SetupError(String),
+    #[error("Store read error: {0}")]
+    StoreReadError(String),
+    #[error("Store parse error: {0}")]
+    StoreParseError(String),
+    #[error("Store write error: {0}")]
+    StoreWriteError(String),
+    #[error("Entry not found: {0}")]
+    EntryNotFound(String),
+    #[error("Entry already exists: {0}")]
+    EntryAlreadyExists(String),
 }
